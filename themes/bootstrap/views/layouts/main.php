bacd64e9402971fff1606e9872f7634e1d3319c0
yurnom <yurnom@126.com>
2014-10-16 14:59:58 +0800

    index change

--- themes/bootstrap/views/layouts/main.php
+++ themes/bootstrap/views/layouts/main.php
@@ -6,28 +6,123 @@
 	<meta name="language" content="en" />
 
     <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/styles.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->baseUrl; ?>/css/docs.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->baseUrl; ?>/css/my.css" />
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 
+	<style>
+	@media (min-width: 1200px) {
+		.mynavbar-fixed-top .container {
+			width: 1170px;
+		}
+	}
+	@media (min-width: 768px) and (max-width: 979px) {
+		.mynavbar-fixed-top .container {
+			width: 1170px;
+		}
+	}
+	</style>
 	<?php Yii::app()->bootstrap->register(); ?>
 </head>
 
-<body>
+<body data-spy="scroll" data-target=".myroll">
 
-<?php $this->widget('bootstrap.widgets.TbNavbar',array(
-    'items'=>array(
-        array(
-            'class'=>'bootstrap.widgets.TbMenu',
-            'items'=>array(
-                array('label'=>'Home', 'url'=>array('/site/index')),
-                array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-                array('label'=>'Contact', 'url'=>array('/site/contact')),
-                array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-                array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
-            ),
-        ),
-    ),
-)); ?>
+<header class="jumbotron subhead" id="overview">
+  <div class="container">
+    <h1>聚博客</h1>
+    <p class="lead">一个优质的IT博客聚集地</p>
+  </div>
+</header>
+<div class="navbar navbar-inverse navbar-fixed-top">
+	<div class="navbar-inner">
+		<div class="container">
+			<a class="btn btn-navbar" data-toggle="collapse" data-target="#yii_bootstrap_collapse_0">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</a>
+			<a class="brand" style="color: #ffffff">聚博客</a>
+			<div class="nav-collapse" id="yii_bootstrap_collapse_0">
+				<ul id="yw0" class="nav">
+					<li class="active"><a href="#">首页</a></li>
+					<li><a href="#">关于本站</a></li>
+					<li><a href="#">意见反馈</a></li>
+				</ul>
+				<form class="navbar-search pull-left" action="">
+					<input type="text" class="search-query span2" placeholder="Search..">
+				</form>
+				<ul class="pull-right nav" id="yw1">
+					<li><a href="#">提交网站</a></li>
+					<li><a href="#">注册</a></li>
+					<li><a href="#">登录</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="navbar mynavbar-fixed-top myroll">
+	<div class="navbar-inner">
+		<div class="container">
+			<a class="btn btn-navbar" data-toggle="collapse" data-target="#yii_bootstrap_collapse_2">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</a>
+			<div class="nav-collapse" id="yii_bootstrap_collapse_2">
+				<ul id="yw50" class="nav">
+					<li><a href="#tbAll">综合</a></li>
+					<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">编程语言 <span class="caret"></span></a>
+						<ul id="yw51" class="dropdown-menu">
+							<li class="nav-header">JVM平台</li>
+							<li><a tabindex="-1" href="#tbJava">Java</a></li>
+							<li><a tabindex="-1" href="#tbScala">Scala</a></li>
+							<li><a tabindex="-1" href="#tbClojure">Clojure</a></li>
+							<li class="divider"></li>
+							<li><a tabindex="-1" href="#tbCCpp">C/C++</a></li>
+							<li><a tabindex="-1" href="#tbCsharp">C#</a></li>
+							<li class="divider"></li>
+							<li><a tabindex="-1" href="#tbPhp">Php</a></li>
+							<li><a tabindex="-1" href="#tbSwift">Swift</a></li>
+						</ul>
+					</li>
+					<li><a href="#tbDatamining">数据挖掘</a></li>
+					<li><a href="#tbBigdata">大数据</a></li>
+					<li><a href="#tbAlgorithm">算法</a></li>
+					<li><a href="#tbML">机器学习</a></li>
+					<li><a href="#tbDatabase">数据库技术</a></li>
+					<li><a href="#tbGame">游戏开发</a></li>
+					<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">移动开发 <span class="caret"></span></a>
+						<ul id="yw52" class="dropdown-menu">
+							<li><a tabindex="-1" href="#tbAndriod">Andriod</a></li>
+							<li><a tabindex="-1" href="#tbiOS">iOS</a></li>
+						</ul>
+					</li>
+					<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">其它 <span class="caret"></span></a>
+						<ul id="yw52" class="dropdown-menu">
+							<li><a tabindex="-1" href="#tbView">可视化</a></li>
+							<li><a tabindex="-1" href="#tbSAS">SAS</a></li>
+						</ul>
+					</li>
+				</ul>
+				<ul class="pull-right nav">
+<!-- 					<li><a href="#tbNew">IT新闻</a></li> -->
+<!-- 					<li><a href="#tbOurstory">程序员那些事</a></li> -->
+					<li><a href="#tbTool">常用工具</a></li>
+					<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">速查手册 <span class="caret"></span></a>
+						<ul id="yw53" class="dropdown-menu">
+							<li><a tabindex="-1" href="#tbMLanguage">编程语言</a></li>
+							<li><a tabindex="-1" href="#tbMFront">前端技术</a></li>
+							<li><a tabindex="-1" href="#tbMDatabase">数据库</a></li>
+							<li><a tabindex="-1" href="#tbMOther">其它</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+
 
 <div class="container" id="page">
 
@@ -38,16 +133,32 @@
 	<?php endif?>
 
 	<?php echo $content; ?>
-
+	
 	<div class="clear"></div>
 
-	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
+</div><!-- page -->
+<footer class="footer">
+	<div class="container">
+		Copyright &copy; <?php echo date('Y'); ?> by BokeList.com.<br/>
 		All Rights Reserved.<br/>
 		<?php echo Yii::powered(); ?>
-	</div><!-- footer -->
-
-</div><!-- page -->
-
+	</div>
+</footer><!-- footer -->
+<!-- Le javascript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="<?php echo Yii::app()->baseUrl; ?>/js/application.js"></script>
+<script src="<?php echo Yii::app()->baseUrl; ?>/js/stickUp.js"></script>
+<script type="text/javascript">
+	//initiating jQuery
+	jQuery(function($) {
+		$(document).ready( function() {
+			//enabling stickUp on the '.navbar-wrapper' class
+			$('.myroll').stickUp({
+				topMargin: '40px'
+			});
+		});
+	});
+</script>
 </body>
 </html>
--- themes/bootstrap/views/site/index.php
+++ themes/bootstrap/views/site/index.php
@@ -4,22 +4,53 @@
 $this->pageTitle=Yii::app()->name;
 ?>
 
-<?php $this->beginWidget('bootstrap.widgets.TbHeroUnit',array(
-    'heading'=>'Welcome to '.CHtml::encode(Yii::app()->name),
-)); ?>
-
-<p>Congratulations! You have successfully created your Yii application.</p>
-
-<?php $this->endWidget(); ?>
-
-<p>You may change the content of this page by modifying the following two files:</p>
-
-<ul>
-    <li>View file: <code><?php echo __FILE__; ?></code></li>
-    <li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
-
-<p>For more details on how to further develop this application, please read
-    the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-    Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-    should you have any questions.</p>
+<div class="container-fluid">
+  <div class="row-fluid">
+    <div class="span9">
+		<!--Body content-->
+	<div class="site-index">
+		<section id="tbAll">
+			<div class="page-header"><h1>综合 <small>添加简介</small></h1></div>
+			<h3>二级导航标题，没有可以去掉</h3>
+			<p>添加导航，具体样式待定
+        </section>
+        
+        <section id="tbJava">
+			<div class="page-header"><h1>编程语言 <small>下面为子类展示方法1</small></h1></div>
+			<h3>Java <small>添加简介</small></h3>
+			<p>添加导航，具体样式待定
+        </section>
+        <section id="tbScala">
+			<h3>Scala <small>添加简介</small></h3>
+			<p>添加导航，具体样式待定
+        </section>
+        <section id="tbClojure">
+			<h3>Clojure <small>添加简介</small></h3>
+			<p>添加导航，具体样式待定
+        </section>
+        
+        <section id="tbJava">
+			<div class="page-header"><h1>移动开发 <small>下面为子类展示方法2</small></h1></div>
+			<div class="tabbable tabs-left"> <!-- Only required for left/right tabs -->
+			  <ul class="nav nav-tabs">
+			    <li class="active"><a href="#tab1" data-toggle="tab">Android</a></li>
+			    <li><a href="#tab2" data-toggle="tab">iOS</a></li>
+			  </ul>
+			  <div class="tab-content">
+			    <div class="tab-pane active" id="tab1">
+			      <p>Android导航</p>
+			    </div>
+			    <div class="tab-pane" id="tab2">
+			      <p>iOS导航</p>
+			    </div>
+			  </div>
+			</div>
+        </section>
+        
+    </div>
+    </div>
+    <div class="span3">
+      <!--Sidebar content-->右侧边栏
+    </div>
+  </div>
+</div>
